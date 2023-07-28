f39db359c89fadbd19ce1dc5b9f9b79fae3491a2
messenger-trevor <messenger.trevor@gmail.com>
2023-07-27 18:35:02 -0600

    adds consistent styling and formatting to contact section to match other sections

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,78 +1,3 @@
-// import { useState } from "react";
-// import { Box, createTheme, TextField, Button, CardHeader } from "@mui/material";
-// import { Send } from "@mui/icons-material";
-// import classes from "./Contact.module.css";
-// import { ThemeProvider } from "@emotion/react";
-
-// const Contact = (props) => {
-//   const [name, setName] = useState(null);
-//   const [email, setEmail] = useState(null);
-//   const [message, setMessage] = useState(null);
-
-//   const theme = createTheme({
-//     palette: {
-//       background: { paper: "#fff" },
-//     }
-//   });
-//   return (
-//     <div className={classes.contactSectionContainer}>
-//       <h1 id="ContactLink" className={classes.sectionHeader}>
-//         Contact Me
-//       </h1>
-//       <ThemeProvider theme={theme}>
-//         <Box 
-//           component="form"
-//           sx={{
-//             bgcolor: "background.paper",
-//             border: ".1rem solid #097179",
-//             borderRadius: 2,
-//             p: 2,
-//           }}
-//         >
-//           <h2>Get In Touch</h2>
-//           <TextField
-//             fullWidth
-//             value={name}
-//             onChange={(event) => {
-//               setName(event.target.value);
-//             }}
-//             label="Name"
-//             id="name"
-//             margin="normal"
-//           ></TextField>
-//           <TextField
-//             fullWidth
-//             value={email}
-//             onChange={(event) => {
-//               setName(event.target.value);
-//             }}
-//             label="Email"
-//             id="email"
-//             margin="normal"
-//           ></TextField>
-//           <TextField
-//             fullWidth
-//             value={message}
-//             onChange={(event) => {
-//               setName(event.target.value);
-//             }}
-//             multiline
-//             rows={4}
-//             label="Message"
-//             id="message"
-//             margin="normal"
-//           ></TextField>
-//           <Button variant="contained" endIcon={<Send />}>
-//             Send
-//           </Button>
-//         </Box>
-//       </ThemeProvider>
-//     </div>
-//   );
-// };
-
-// export default Contact;
-
 import { useState } from "react";
 import { Box, createTheme, TextField, Button, CardHeader } from "@mui/material";
 import { Send } from "@mui/icons-material";
@@ -92,13 +17,13 @@ const Contact = (props) => {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Perform your logic here with the form data
+
     console.log("Form submitted!");
     console.log("Name:", name);
     console.log("Email:", email);
     console.log("Message:", message);
 
-    // Reset the form after submission (optional)
+    // Resets the form
     setName("");
     setEmail("");
     setMessage("");
@@ -106,6 +31,15 @@ const Contact = (props) => {
 
   return (
     <div className={classes.contactSectionContainer}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          direction: "ltr",
+          alignItems: "center",
+          overflow: "hidden",
+          // width: "500px",
+        }}>
       <h1 id="ContactLink" className={classes.sectionHeader}>
         Contact Me
       </h1>
@@ -158,6 +92,7 @@ const Contact = (props) => {
           </Button>
         </Box>
       </ThemeProvider>
+      </div>
     </div>
   );
 };
